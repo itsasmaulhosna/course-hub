e3016bd703e270848c7115fd4735c8950c405cb9
itsasmaulhosna <itsasmaulhosna@gmail.com>
2026-07-13 10:44:47 +0600

    implemented jwt

--- src/app/api/courses/route.ts
+++ src/app/api/courses/route.ts
@@ -1,16 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
 import client from "@/lib/mongodb";
+import { requireAuth } from "@/lib/requireAuth";
 
+// Create Course (Protected)
 export async function POST(req: NextRequest) {
   try {
+    // Login user check
+    const session = await requireAuth();
+
     const body = await req.json();
 
     const db = client.db("course-hub");
-
     const collection = db.collection("courses");
 
     const result = await collection.insertOne({
       ...body,
+      createdBy: session.user.id,
+      createdByEmail: session.user.email,
       createdAt: new Date(),
       updatedAt: new Date(),
     });
@@ -19,8 +25,21 @@ export async function POST(req: NextRequest) {
       success: true,
       insertedId: result.insertedId,
     });
+
   } catch (error) {
-    console.log(error);
+    console.error(error);
+
+    if (error instanceof Error && error.message === "Unauthorized") {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Unauthorized",
+        },
+        {
+          status: 401,
+        }
+      );
+    }
 
     return NextResponse.json(
       {
@@ -33,22 +52,39 @@ export async function POST(req: NextRequest) {
     );
   }
 }
-// get all courses
+
+// Get All Courses (Protected)
 export async function GET() {
   try {
-    const db = client.db("course-hub");
+    // Login check
+    await requireAuth();
 
+    const db = client.db("course-hub");
     const collection = db.collection("courses");
 
     const courses = await collection.find().toArray();
 
     return NextResponse.json(courses);
+
   } catch (error) {
-    console.log(error);
+    console.error(error);
+
+    if (error instanceof Error && error.message === "Unauthorized") {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Unauthorized",
+        },
+        {
+          status: 401,
+        }
+      );
+    }
 
     return NextResponse.json(
       {
         success: false,
+        message: "Something went wrong",
       },
       {
         status: 500,
